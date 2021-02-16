6b0290159850a5f81cf1f727bceac1af4ccb526e
alvaroaranda05 <alvaro.aranda05@gmail.com>
2021-02-16 01:57:41 +0100

    Add description to project response

--- src/main/java/com/varitoooo/criteria/project/application/ProjectResponse.java
+++ src/main/java/com/varitoooo/criteria/project/application/ProjectResponse.java
@@ -3,10 +3,12 @@ package com.varitoooo.criteria.project.application;
 public final class ProjectResponse {
     private final int id;
     private final String name;
+    private final String description;
 
-    public ProjectResponse(int id, String name) {
+    public ProjectResponse(int id, String name, String description) {
         this.id = id;
         this.name = name;
+        this.description = description;
     }
 
     public int getId() {
@@ -15,5 +17,9 @@ public final class ProjectResponse {
 
     public String getName() {
         return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
--- src/main/java/com/varitoooo/criteria/project/application/ProjectResponseConverter.java
+++ src/main/java/com/varitoooo/criteria/project/application/ProjectResponseConverter.java
@@ -5,8 +5,8 @@ import com.varitoooo.criteria.project.domain.Project;
 public final class ProjectResponseConverter {
     public ProjectResponse getResponse(Project project) {
         return new ProjectResponse(
-            project.getId().getValue(),
-            project.getName().getValue()
-        );
+                project.getId().getValue(),
+                project.getName().getValue(),
+                project.getDescription().getValue());
     }
 }
